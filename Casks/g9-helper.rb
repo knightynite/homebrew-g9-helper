fdbe6864ef2d6ea136c5720c9ad09b3eaadcaf4b
AL <76608874+knightynite@users.noreply.github.com>
2026-03-23 08:27:24 -0500

    Update cask to v1.1.5

--- Casks/g9-helper.rb
+++ Casks/g9-helper.rb
@@ -1,6 +1,6 @@
 cask "g9-helper" do
-  version "1.1.1"
-  sha256 "9b4f076be79b9a0b63be02710aa01501e2238c08d581778cd5dcaacbe8f35896"
+  version "1.1.5"
+  sha256 "d97060effce4447ed1b728f509bf3031d2ea3c3c4c0b312298fad33ec7cd9655"
 
   url "https://github.com/knightynite/HiDPIVirtualDisplay/releases/download/v#{version}/G9.Helper-v#{version}.dmg"
   name "G9 Helper"
