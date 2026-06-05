b305b0d6d6f2c6037768f31a2c99cc0cd6f9b83f
al najafi <g9helper@alcybr.com>
2026-06-05 00:43:08 -0500

    g9-helper 1.2.1
    Update cask to v1.2.1 (universal binary, Intel support).

--- Casks/g9-helper.rb
+++ Casks/g9-helper.rb
@@ -1,6 +1,6 @@
 cask "g9-helper" do
-  version "1.2.0"
-  sha256 "2de27a8982a99a043179d5613d557df71fdb17d5168c16d46350473b1f5640ce"
+  version "1.2.1"
+  sha256 "c52776ffedb995b611539fd3c9fdf96a9593bbecc81e60eb4a45cf4c2e01d6ce"
 
   url "https://github.com/knightynite/HiDPIVirtualDisplay/releases/download/v#{version}/G9.Helper-v#{version}.dmg"
   name "G9 Helper"
